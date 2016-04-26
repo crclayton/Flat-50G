2dfeeebb3b35a490d24c312acc3d5db2449a13bf
Charles Clayton <charlesrussellclayton@gmail.com>
2016-04-26 11:18:25 -0700

    Added some more shortcuts. Trying to export image.

---
+++ CalculatorBackground/CalculatorBackground/app.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+    $(window).load(function () {
+
+        var scaleBy = 5;
+        var w = 892;
+        var h = 590;
+        var div = document.querySelector('#screen');
+        var canvas = document.createElement('canvas');
+        canvas.width = w;
+        canvas.height = h;
+
+        canvas.style.width = w + 'px';
+        canvas.style.height = h + 'px';
+        canvas.style.border = "1px solid black";
+
+        var context = canvas.getContext('2d');
+        context.scale(scaleBy, scaleBy);
+
+
+        html2canvas(div, {
+            canvas:canvas,
+            onrendered: function (canvas) {
+                theCanvas = canvas;
+                document.body.appendChild(canvas);
+                //Canvas2Image.saveAsBMP(canvas);
+                $(body).append(canvas);
+            }
+           
+        });
+    });
+});
